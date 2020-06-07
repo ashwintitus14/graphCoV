1d21f2b810f65c142f0ea94b271b515bce27adc3
ashwintitus14 <ashwintitus14@gmail.com>
2020-06-07 12:31:03 +0530

    Created models

--- gen_graph/models.py
+++ gen_graph/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+
+    p_id = models.CharField(max_length=10, help_text='Patient/Contact ID', primary_key=True)
+    tested_positive = models.BooleanField(default=False, help_text='True if person is COVID 19 positive')
+
+class Links(models.Model):
+
+    person1 = models.CharField(max_length=10, help_text='Patient/Contact ID of person 1')
+    person2 = models.CharField(max_length=10, help_text='Patient/Contact ID of person 2')
